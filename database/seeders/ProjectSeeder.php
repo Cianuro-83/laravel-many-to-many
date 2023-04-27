a59fa40cffdb0332a81d8878afd00d4e4048a1e4
Cianuro-83 <buzzeosimone@yahoo.com>
2023-04-27 16:21:41 +0200

    FEAT: associo tecnologie e progetti

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -8,6 +8,7 @@ use Faker\Generator as Faker;
 use Illuminate\Support\Str;
 use App\Models\Project;
 use App\Models\Type;
+use App\Models\Technology;
 
 class ProjectSeeder extends Seeder
 {
@@ -19,6 +20,8 @@ class ProjectSeeder extends Seeder
     public function run(Faker $faker)
     {
         $type_ids = Type::all()->pluck('id')->all();
+        $technology_ids = Technology::all()->pluck('id')->all();
+
         for ($i = 0; $i < 10; $i++){
             $project = new Project();
             $project->title = $faker->unique()->sentence($faker->numberBetween(2, 3));
@@ -28,6 +31,8 @@ class ProjectSeeder extends Seeder
             $project->slug = Str::slug($project->title, '-');
             $project->type_id = $faker->optional()->randomElement($type_ids);
             $project->save();
+
+            $project->technologies()->attach($faker->randomElements($technology_ids, rand(0,3)));
 
         }
     }
